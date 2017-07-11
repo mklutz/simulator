1e8bd75f13b2b5c8293495098a1d14d6f242501e
Michael Lutz <michael@mklutz.de>
2017-07-11 22:20:40 +0200

    Ball in Schritt und Lauf

--- Simulator/appClientModule/de/mklutz/simulator/gui/Simulator.java
+++ Simulator/appClientModule/de/mklutz/simulator/gui/Simulator.java
@@ -1,17 +1,25 @@
 package de.mklutz.simulator.gui;
 
 import java.awt.EventQueue;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
 
-public class Simulator {
+import de.mklutz.simulator.model.Ball;
+
+public class Simulator implements Runnable {
 
 	private JFrame frame;
-	private JTable table;
+	JPanel panel;
+	JSlider warteZeit;
+	private boolean laufModus = false;
+	long pause = 1;
 
 	/**
 	 * Launch the application.
@@ -46,22 +54,73 @@ public class Simulator {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		JButton stop = new JButton("Stop");
-		stop.setBounds(232, 11, 89, 23);
-		frame.getContentPane().add(stop);
-
-		JButton start = new JButton("Start");
-		start.addActionListener(new ActionListener() {
+		JButton lauf = new JButton("Lauf");
+		lauf.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
+				laufModus = true;
+				berechneUndZeige();
 			}
 		});
-		start.setBounds(133, 11, 89, 23);
-		frame.getContentPane().add(start);
+		lauf.setBounds(309, 11, 89, 23);
+		frame.getContentPane().add(lauf);
 
-		table = new JTable();
-		table.setBounds(31, 47, 367, 188);
-		frame.getContentPane().add(table);
+		JButton schritt = new JButton("Schritt");
+		schritt.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				laufModus = false;
+				berechneUndZeige();
+			}
+		});
 
+		schritt.setBounds(210, 11, 89, 23);
+		frame.getContentPane().add(schritt);
+
+		warteZeit = new JSlider();
+		warteZeit.setBounds(33, 11, 167, 26);
+		frame.getContentPane().add(warteZeit);
+
+		panel = new JPanel();
+		panel.setBounds(22, 56, 381, 176);
+		frame.getContentPane().add(panel);
+
+		JLabel lblSchnell = new JLabel("schnell");
+		lblSchnell.setBounds(10, 0, 46, 14);
+		frame.getContentPane().add(lblSchnell);
+
+		JLabel lblLangsam = new JLabel("langsam");
+		lblLangsam.setBounds(156, 0, 46, 14);
+		frame.getContentPane().add(lblLangsam);
+
+	}
+
+	void berechneUndZeige() {
+
+		Ball ball = Ball.newBall();
+
+		print(panel.getGraphics(), ball);
+
+		if (laufModus) {
+			try {
+				Thread.sleep(warteZeit.getValue() * 10);
+				EventQueue.invokeLater(this);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	void print(Graphics gr, Ball ball) {
+		gr.drawOval(ball.getX(), ball.getY(), ball.getRadius(), ball.getRadius());
+	}
+
+	@Override
+	public void run() {
+		berechneUndZeige();
+
+		// if (laufModus) {
+		// EventQueue.invokeLater(this);
+		// }
 	}
 }
